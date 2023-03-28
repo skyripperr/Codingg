21d886040a019a420b281c7ed798a6fbb2d1d39e
Zaync_Zero <129195372+skyripperr@users.noreply.github.com>
2023-03-29 00:10:49 +0700

    Add files via upload

---
+++ multilogin/cek_login.php
@@ -0,0 +1,57 @@
+<?php 
+// mengaktifkan session pada php
+session_start();
+ 
+// menghubungkan php dengan koneksi database
+include 'function.php';
+ 
+// menangkap data yang dikirim dari form login
+$username = $_POST['username'];
+$password = $_POST['password'];
+ 
+ 
+// menyeleksi data user dengan username dan password yang sesuai
+$login = mysqli_query($koneksi,"select * from login where username='$username' and password='$password'");
+// menghitung jumlah data yang ditemukan
+$cek = mysqli_num_rows($login);
+ 
+// cek apakah username dan password di temukan pada database
+if($cek > 0){
+ 
+	$data = mysqli_fetch_assoc($login);
+ 
+	// cek jika user login sebagai admin
+	if($data['level']=="admin"){
+ 
+		// buat session login dan username
+		$_SESSION['username'] = $username;
+		$_SESSION['level'] = "admin";
+		// alihkan ke halaman dashboard admin
+		header("location:halaman_admin.php");
+ 
+	// cek jika user login sebagai petugas
+	}else if($data['level']=="petugas"){
+		// buat session login dan username
+		$_SESSION['username'] = $username;
+		$_SESSION['level'] = "petugas";
+		// alihkan ke halaman dashboard petugas
+		header("location:halaman_petugas.php");
+ 
+	// cek jika user login sebagai pengurus
+	}else if($data['level']=="masyarakat"){
+		// buat session login dan username
+		$_SESSION['username'] = $username;
+		$_SESSION['level'] = "masyarakat";
+		// alihkan ke halaman dashboard pengurus
+		header("location:masyarakat.php");
+ 
+	}else{
+ 
+		// alihkan ke halaman login kembali
+		header("location:index.php?pesan=gagal");
+	}	
+}else{
+	header("location:index.php?pesan=gagal");
+}
+ 
+?>
